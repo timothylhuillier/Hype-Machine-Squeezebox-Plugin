6be0a80aace45e5b0a6c2f44d691268b9ae779df
David Blackman <blackmad@foursquare.com>
2012-02-26 16:46:24 -0500

    HypeM

--- generate-zip.sh
+++ generate-zip.sh
@@ -1,8 +1,8 @@
 set -x
 cd ..
-zip -r Hype\ Machine Hype\ Machine -x \*.zip \*.sh \*.git\* \*README\* \*webauth\*
-mv Hype\ Machine.zip Hype\ Machine
-cd Hype\ Machine
+zip -r HypeM HypeM -x \*.zip \*.sh \*.git\* \*README\* \*webauth\*
+mv HypeM.zip HypeM
+cd HypeM
 
 VERSION=$(grep \<version\> install.xml  | perl -n -e '/>(.*)</; print $1;')
 SHA=$(shasum HypeM.zip | awk '{print $1;}')
@@ -13,9 +13,9 @@ cat <<EOF > public.xml
 <title lang="EN">Whizziwig's Plugins</title>
 </details>
 <plugins>
-<plugin name="Hype Machine" version="$VERSION" minTarget="7.5" maxTarget="*">
+<plugin name="HypeM" version="$VERSION" minTarget="7.5" maxTarget="*">
 <title lang="EN">Hype Machine</title>
-<desc lang="EN">Browse, search and play urls from soundcloud</desc>
+<desc lang="EN">Browse, search and play urls from The Hype Machine</desc>
 <url>http://whizziwig.com/static/hypebox/HypeM.zip</url>
 <link>https://github.com/blackmad/Hype-Machine-Squeezebox-Plugin</link>
 <sha>$SHA</sha>
