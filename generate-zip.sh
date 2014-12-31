9b3161783738d8211c5ee9377e02dd1f2121ec88
Timothy L'HUILLIER <timothy.lhuillier@gmail.com>
2014-12-31 16:03:26 +0100

    generate-zip : s'inspire du code de SqueezeCloud et je l'ai modifié

--- generate-zip.sh
+++ generate-zip.sh
@@ -3,17 +3,18 @@ set -x
 # recupère la version inscrite dans install.xml avant de changer de dossier
 VERSION=$(grep \<version\> install.xml  | perl -n -e '/>(.*)</; print $1;')
 
-# créer un dossier temporaire qui va contenir le zip
-mkdir generateTpl
-cd generateTpl
-# télécharge le zip pour le hasher
-wget https://github.com/timothylhuillier/Hype-Machine-Squeezebox-Plugin/archive/master.zip
-# hash le zip et stock la valeur dans la variable 'SHA'
-SHA=$(shasum master.zip | awk '{print $1;}')
-
-# retour dans le dossier parent et supprime le dossier temporaire
+# rejoins le dossier parent et créer un dossier contenant les zips
 cd ../
-rm -rf generateTpl
+mkdir generateZip
+cd generateZip
+
+# zip le contenu du plugin
+zip -r HypeM-$VERSION.zip ../HypeM -x \*.zip \*.sh \*.git\* \*README\* \*webauth\*
+# hash le zip et stock la valeur dans la variable 'SHA'
+SHA=$(shasum HypeM-$VERSION.zip | awk '{print $1;}')
+
+# retour dans le dossier du plugin
+cd ../HypeM
 
 # Mets à jour le fichier repo.xml
 cat <<EOF > repo.xml
@@ -24,10 +25,10 @@ cat <<EOF > repo.xml
 		<title lang="EN">timothylhuillier's Plugins</title>
 	</details>
 	<plugins>
-		<plugin name="HypeM" version="$VERSION" minTarget="7.0" maxTarget="7.*">
+		<plugin name="HypeM" version="$VERSION" minTarget="7.5" maxTarget="7.*">
 			<title lang="EN">Hype Machine</title>
 			<desc lang="EN">A plugin for the logitech meda server, HypeM is a platform of streamin music.</desc>
-			<url>http://paperops.fr/lms/hypem.zip</url>
+			<url>http://paperops.fr/lms/HypeM-$VERSION.zip</url>
 			<sha>$SHA</sha>
 			<link>https://github.com/timothylhuillier/Hype-Machine-Squeezebox-Plugin</link>
 			<creator>David BLACKMAN and Timothy L'HUILLIER</creator>
@@ -36,4 +37,3 @@ cat <<EOF > repo.xml
 	</plugins>
 </extensions>
 EOF
-
